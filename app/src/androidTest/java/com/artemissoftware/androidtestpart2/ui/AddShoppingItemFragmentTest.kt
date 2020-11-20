b395b816b9a8262021d7db8be12570b3bab8bf2a
gustavo <gustavo.maia@b2b.com.pt>
2020-11-20 15:51:22 +0000

    Test Addition of Shopping Items

--- app/src/androidTest/java/com/artemissoftware/androidtestpart2/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/artemissoftware/androidtestpart2/ui/AddShoppingItemFragmentTest.kt
@@ -1,5 +1,6 @@
 package com.artemissoftware.androidtestpart2.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.filters.MediumTest
@@ -16,9 +17,17 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.artemissoftware.androidtestpart2.R
+import com.artemissoftware.androidtestpart2.data.local.ShoppingItem
+import com.artemissoftware.androidtestpart2.getOrAwaitValue
 import com.artemissoftware.androidtestpart2.launchFragmentInHiltContainer
+import com.artemissoftware.androidtestpart2.repositories.FakeShoppingRepositoryAndroidTest
 import org.junit.Test
+import javax.inject.Inject
+
+import com.google.common.truth.Truth.assertThat
 
 @MediumTest
 @HiltAndroidTest
@@ -30,10 +39,40 @@ class AddShoppingItemFragmentTest{
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
     @Before
     fun setUp(){
         hiltRule.inject()
     }
+
+
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("shopping item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("5.5"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem("shopping item", 5, 5.5f, ""))
+    }
+
 
     @Test
     fun pressBackButton_popBackStack() {
